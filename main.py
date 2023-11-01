8e09d698d079dabddaa14ac05ba8f23dcf571d4f
adryanmaikel <adryanmaikel@gmail.com>
2023-10-31 22:26:42 -0300

    finalizando backend

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def transform_in_timedelta(hour):
   return timedelta(hours=hours, minutes=minutes)
 
 
-def format_time(time):
+def formatting_in_time(time):
   time = str(time) if not type(time) == type('') else time
   time = time.split(', ')[1] if time.count(',') == 1 else time
   time = f'{time}:00' if time.count(':') == 1 else time
   return datetime.strptime(time, "%H:%M:%S").strftime("%H:%M")
-# print(format_time('1 day, 0:49:00')) print(format_time('7:10:00'))
+# print(formatting_in_time('1 day, 0:49:00')) -> '00:49' print(formatting_in_time('7:10:00')) -> 07:10
 
 
 def calculate_hour_exit(
@@ -27,13 +27,18 @@ def calculate_hour_exit(
   end_journey = start_journey + total_journey
   
   return print(
-    f'Duração do Intervalo: {format_time(total_interval)}\n'
-    f'Soltará às: {format_time(end_journey)}'
+    f'Duração do Intervalo: {formatting_in_time(total_interval)}\n'
+    f'Soltará às: {formatting_in_time(end_journey)}'
   )
 
 
+def formatting_in_hour(time):
+  return f'{time[0:2]}:{time[2:4]}' if len(time) == 4 else time
+#formatting_time('1520') -> '15:20'
+
+
 calculate_hour_exit(
-  start_journey=input('Início da jornada: '),
-  start_interval=input('Início do intervalo: '),
-  end_interval=input('Fim do intervalo: ')
+  start_journey=formatting_in_hour(input('Início da jornada: ')),
+  start_interval=formatting_in_hour(input('Início do intervalo: ')),
+  end_interval=formatting_in_hour(input('Fim do intervalo: '))
 )
