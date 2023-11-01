7f551af301efeec2c52c98b43e38a8716499077e
adryanmaikel <adryanmaikel@gmail.com>
2023-11-01 00:29:57 -0300

    criando front

---
+++ app.py
@@ -0,0 +1,4 @@
+
+from main import calculate_hour_exit
+
+calculate_hour_exit()
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from datetime import timedelta, datetime
 
-def transform_in_timedelta(hour):
-  hours, minutes = [int(i) for i in str(hour).split(':')]
+def transform_in_timedelta(time):
+  print(len(time))
+  time = str(time) if not type(time) == type('string') else time
+  time = f'0{time[0]}:{time[1:]}' if time.count(':') == 0 and len(time) == 3 else time
+  print(time)
+  time = f'{time[0:2]}:{time[2:4]}' if time.count(':') == 0 and len(time) == 4 else time
+  print(time)
+  
+  hours, minutes = [int(i) for i in time.split(':')]
   return timedelta(hours=hours, minutes=minutes)
 
 
-def formatting_in_time(time):
-  time = str(time) if not type(time) == type('') else time
+def format_time(time):
+  time = str(time) if not type(time) == type('string') else time
   time = time.split(', ')[1] if time.count(',') == 1 else time
   time = f'{time}:00' if time.count(':') == 1 else time
   return datetime.strptime(time, "%H:%M:%S").strftime("%H:%M")
-# print(formatting_in_time('1 day, 0:49:00')) -> '00:49' print(formatting_in_time('7:10:00')) -> 07:10
+# print(format_time('1 day, 0:49:00')) print(format_time('7:10:00'))
 
 
 def calculate_hour_exit(
-    journey='7:10', start_journey='15:49',
+    journey='710', start_journey='1549',
     start_interval='17:00', end_interval='17:30'
   ):
   journey = transform_in_timedelta(journey)
@@ -26,19 +33,15 @@ def calculate_hour_exit(
   total_journey = journey + total_interval
   end_journey = start_journey + total_journey
   
-  return print(
-    f'Duração do Intervalo: {formatting_in_time(total_interval)}\n'
-    f'Soltará às: {formatting_in_time(end_journey)}'
+  return (
+    f'Duração do Intervalo: {format_time(total_interval)}\n'
+    f'Soltará às: {format_time(end_journey)}'
   )
+  return result
 
 
-def formatting_in_hour(time):
-  return f'{time[0:2]}:{time[2:4]}' if len(time) == 4 else time
-#formatting_time('1520') -> '15:20'
-
-
-calculate_hour_exit(
-  start_journey=formatting_in_hour(input('Início da jornada: ')),
-  start_interval=formatting_in_hour(input('Início do intervalo: ')),
-  end_interval=formatting_in_hour(input('Fim do intervalo: '))
-)
+# calculate_hour_exit(
+#   start_journey=input('Início da jornada: '),
+#   start_interval=input('Início do intervalo: '),
+#   end_interval=input('Fim do intervalo: ')
+# )
